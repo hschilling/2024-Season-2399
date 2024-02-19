34bcd54984f3c48536d335ab30e14c1fe5ef89d0
Aashvi Jagetia <ajagetia26@hb.edu>
2024-02-19 14:04:05 -0500

    switching branches to io vision branch

---
+++ src/main/java/frc/robot/commands/AlignAprilTag.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Vision;
+import frc.robot.Constants.VisionConstants;
+
+
+public class AlignAprilTag extends Command {
+  /** Creates a new AlignAprilTag. */
+  public AlignAprilTag() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
